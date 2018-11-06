3e70fec3dc0b1559b7aec2f30dff9341d9859eb4
Andrew Schwartz <andrew@aschwartz.me>
2018-11-05 19:42:16 -0700

    implement multi year graphing data

--- open_data.py
+++ open_data.py
@@ -2,6 +2,8 @@ import sqlite3
 from sqlite3 import Error
 import matplotlib.pyplot as plt
 from adjustText import adjust_text
+import numpy as np
+from scipy import stats
 
 
 teams = [
@@ -102,30 +104,68 @@ def graph_team_data(teams, margins, positions, season):
 	plt.show()
 
 
+def graph_multi_year_data(teams_list, margins_list, positions_list, seasons_list):
+	fig, ax = plt.subplots()
+	# fig, ax = plt.subplots(figsize=(18,9))
+	colors = ['b', 'g', 'r', 'c', 'm', 'y']
+	for i in range(0, len(seasons_list)):
+		label = seasons_list[i]
+		ax.scatter(
+			margins_list[i],
+			positions_list[i],
+			c=colors[i],
+			alpha=0.6,
+			label=seasons_list[i]
+		)
+		[plt.text(margins_list[i][j], positions_list[i][j], teams_list[i][j]) for j in range(len(teams_list[i]))]  # add labels
+
+		gradient, intercept, r_value, p_value, std_err = stats.linregress(margins_list[i], positions_list[i])
+		mn = np.min(margins_list[i])
+		mx = np.max(margins_list[i])
+		x1 = np.linspace(mn, mx, 500)
+		y1 = gradient * x1 + intercept
+		plt.plot(x1, y1, colors[i])
+
+
+	plt.legend(loc='upper left')
+	plt.title("Average Point Win Margin vs. Playoff Postions ({0}-{1})".format(min(seasons_list), max(seasons_list)))
+	plt.ylabel("Playoff Value*")
+	plt.xlabel("Avg. Win Margin")
+	plt.show()
+
+
 def main():
 	database = "C:\\Users\\werdn\\Documents\\NBA-math-IA\\nba-data.db"
 
 	# create a database connection
 	conn = create_connection(database)
-	teams_to_graph = []
-	margins_to_graph = []
-	playoff_postions = []
-	season = 2018
+	season_min = 2017
+	season_max = 2018
+	teams_list = []
+	margins_list = []
+	playoff_positions_list = []
+	seasons_list = []
 	with conn:
-		for team in teams:
-			rows = get_team_games(conn, team, season)
-			if rows is not None:
-				margin = get_winning_margins(rows, team)
-				teams_to_graph.append(team)
-				margins_to_graph.append(margin)
-				playoff_pos = get_playoff_position(conn, team, season)
-				playoff_postions.append(playoff_pos)
-				# print("playoff position: ", playoff_pos)
-				# print("========================")
-	# print(teams_to_graph)
-	# print(margins_to_graph)
-	# print(playoff_postions)
-	graph_team_data(teams_to_graph, margins_to_graph, playoff_postions, season)
+		for season in range(season_min, season_max + 1):
+			teams_to_graph = []
+			margins_to_graph = []
+			playoff_postions = []
+			print('in season ', season, '...')
+			for team in teams:
+				rows = get_team_games(conn, team, season)
+				if rows is not None:
+					margin = get_winning_margins(rows, team)
+					teams_to_graph.append(team)
+					margins_to_graph.append(margin)
+					playoff_pos = get_playoff_position(conn, team, season)
+					playoff_postions.append(playoff_pos)
+					# print("playoff position: ", playoff_pos)
+					# print("========================")
+			teams_list.append(teams_to_graph)
+			margins_list.append(margins_to_graph)
+			playoff_positions_list.append(playoff_postions)
+			seasons_list.append(season)
+	graph_multi_year_data(teams_list, margins_list, playoff_positions_list, seasons_list)
 
 
 if __name__ == '__main__':
